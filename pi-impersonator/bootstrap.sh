44b36c0c1f4379a3d16ab1709248203786f0dcc3
bullitt186 <bastian@stahmer.net>
2026-09-19 13:18:07 +0200

    bootstrap: install gstreamer1.0-nice (WebRTC ICE plugin)
    webrtcbin cannot do ICE without the GStreamer 'nice' plugin (libgstnice.so);
    without it webrtcbin fails to link H264 RTP. Add it to the apt dependency list.

--- pi-impersonator/bootstrap.sh
+++ pi-impersonator/bootstrap.sh
@@ -24,7 +24,7 @@ fi
 log "apt: gstreamer + libcamera/rpicam + python-gi + venv tooling"
 "${SSH[@]}" 'sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
   python3-gi python3-gst-1.0 gstreamer1.0-tools gstreamer1.0-plugins-base \
-  gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-rtsp rpicam-apps \
+  gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-nice gstreamer1.0-rtsp rpicam-apps \
   gir1.2-gst-rtsp-server-1.0 gir1.2-gst-plugins-bad-1.0 \
   python3-venv python3-pip rsync'
 
